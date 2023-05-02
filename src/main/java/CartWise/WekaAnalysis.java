64856c513886db5f6ba5cf37e2fc8fc358e9d49e
Mohamed Bekour <mohamed.bekour@stud.hs-emden-leer.de>
2023-05-02 05:19:13 +0200

    The methode StaerksteZeitAmTag added

--- src/main/java/CartWise/WekaAnalysis.java
+++ src/main/java/CartWise/WekaAnalysis.java
@@ -21,14 +21,17 @@ public class WekaAnalysis {
         data = loader.getDataSet();
     }
 
-    // Methode, um die Tagesumsätze zu berechnen
+    // Methode, um die Tagesums�tze zu berechnen
     public Map<String, Double> getTaglichDaten() {
         Map<String, Double> gesamtProTag = new HashMap<>();
         int dayIndex = data.attribute("Einkaufstag").index();
         int salesIndex = data.attribute("Einkaufssumme").index();
+        
+
 
         // Durchlaufen aller Instanzen und Aufsummieren der Tagesumsätze
         for (int i = 0; i < data.numInstances(); i++) {
+        	
             String day = data.instance(i).stringValue(dayIndex);
             double sales = data.instance(i).value(salesIndex);
 //            System.out.println("days: " + day);
@@ -40,7 +43,7 @@ public class WekaAnalysis {
         return gesamtProTag;
     }
 
-    // Methode, um die stündlichen Umsätze zu berechnen
+    // Methode, um die st�ndlichen Ums�tze zu berechnen
     public Map<String, Double> getGesamtroStunde() {
         Map<String, Double> gesamtProStunde = new HashMap<>();
         int uhrzeitIndex = data.attribute("Einkaufsuhrzeit").index();
@@ -57,30 +60,68 @@ public class WekaAnalysis {
         return gesamtProStunde;
     }
 
-    // Methode, um den Tag mit dem höchsten Umsatz zu ermitteln
+    // Methode, um den Tag mit dem h�chsten Umsatz zu ermitteln
     public String getTopDay() {
         Map<String, Double> gesamtProTag = getTaglichDaten();
         return Collections.max(gesamtProTag.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
 
-    // Methode, um die Stunde mit dem höchsten Umsatz zu ermitteln
+    // Methode, um die Stunde mit dem h�chsten Umsatz zu ermitteln
     public String getTopHour() {
         Map<String, Double> gesamtProStunde = getGesamtroStunde();
         return Collections.max(gesamtProStunde.entrySet(), Map.Entry.comparingByValue()).getKey();
+    }
+    
+    
+    // Methode, die st�rkste Einkaufsuhrzeit an den einzelnen Tagen ermitteln
+    public Map<String, String> StaerksteZeitAmTag() {
+    	
+        Map<String,String> map = new HashMap<>();
+        Map<String,Double> map2 = new HashMap<>();
+        
+  
+        
+        int tagIndex = data.attribute("Einkaufstag").index();
+        int uhrzeitIndex = data.attribute("Einkaufsuhrzeit").index();
+        int salesIndex = data.attribute("Einkaufssumme").index();
+
+        // Durchlaufen aller Instanzen und Aufsummieren der stündlichen Umsätze
+        for (int i = 0; i < data.numInstances(); i++) {
+        	
+        	String tag = data.instance(i).stringValue(tagIndex);
+            String uhrzeit = data.instance(i).stringValue(uhrzeitIndex);
+            double sales = data.instance(i).value(salesIndex);
+
+            //map.put(tag,Math.max(map.getOrDefault(tag, 0.0), sales) );
+            
+            
+            if( sales > map2.getOrDefault(tag, 0.0)) {
+            	map2.put(tag,sales);
+            	map.put(tag,uhrzeit); 
+            }
+   
+        }
+
+        return map;
     }
 
     // Hauptmethode
     public static void main(String[] args) {
         try {
             // Erstellen eines WekaAnalysis-Objekts
-            WekaAnalysis analysis = new WekaAnalysis("/Users/smallwest888/Downloads/kd10000.csv");
+            WekaAnalysis analysis = new WekaAnalysis("C:\\Users\\moham\\Desktop\\kd100.csv");
             // Ermitteln des Tages und der Stunde mit dem höchsten Umsatz
             String topDay = analysis.getTopDay();
             String topHour = analysis.getTopHour();
+            
+            Map<String,String> mapTest = analysis.StaerksteZeitAmTag();
+            
+            
 
-                        // Ausgabe der Ergebnisse
+            // Ausgabe der Ergebnisse
             System.out.println("umsatzstärkster Einkaufstag: " + topDay);
             System.out.println("Umsatzstärkste Einkaufsuhrzeit: " + topHour);
+            System.out.println(mapTest);
 
         } catch (IOException e) {
             // Behandlung von möglichen Eingabe-/Ausgabe-Ausnahmen
